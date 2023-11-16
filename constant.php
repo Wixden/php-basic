c547ae6ad022e29b0ebf3914b12e1688d2e61383
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-16 23:39:04 +0600

    constant variables in php

---
+++ constant.php
@@ -0,0 +1,22 @@
+<?php
+// To create a constant, use the define() function
+// Create a constant with a case-sensitive name:
+define("name", "Akhtar Hussain");
+
+
+// Create a constant with a case-insensitive name:
+define("email", "akhtarhussain97@gmail.com", true);
+
+/* 
+Parameters:
+
+name: Specifies the name of the constant
+value: Specifies the value of the constant
+case-insensitive: Specifies whether the constant name should be case-insensitive. Default is false. Note: Defining case-insensitive constants was deprecated in PHP 7.3. PHP 8.0 accepts only false, the value true will produce a warning.
+
+*/
+
+// You can also create a constant by using the const keyword.
+
+const firstName = "Mohammed";
+const lastName = "Akhtar Hussain";
